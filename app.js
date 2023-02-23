936e411822b95b5ad427e7d3eb10b13eabc86bb5
Admin <admin@Admins-MacBook-Pro.local>
2023-02-23 11:27:47 -0800

    beginnings

---
+++ app.js
@@ -0,0 +1,44 @@
+// unit_1 project
+
+
+$(() => {
+
+//biography that goes down
+//carousel for projects
+
+    let currentIndex = 0;
+
+    const num = $('.projects').children().length - 1
+
+    $('.next').on('click', () => {
+       
+        $('.projects').children().eq(currentIndex).css('display', 'none');
+        
+        if(currentIndex < num) {
+            currentIndex++
+           } else {
+            currentImgIndex = 0
+           }
+
+        $('.projects').children().eq(currentIndex).css('display', 'block');
+       })
+
+$('.previous').on('click', () => {
+        
+        $('.projects').children().eq(currentIndex).css('display', 'none');
+        
+        if(currentIndex > 0) {
+            currentIndex --
+           } else {
+            currentIndex = num
+           }
+
+        $('.projects').children().eq(currentIndex).css('display', 'block');
+       })
+
+
+    //    $('div').on('click', function() {
+    //     $(this).toggleClass('show-description');
+    // });
+
+});
